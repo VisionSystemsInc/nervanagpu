07d3a714dbea0b825b1c1a85cbb8c45cf9976bec
Alex Park <alex.park@gmail.com>
2015-07-14 16:36:38 -0700

    Fixed bad syntax in tensor construction

--- nervanagpu/nervanagpu.py
+++ nervanagpu/nervanagpu.py
@@ -543,31 +543,35 @@ class NervanaGPU(object):
         self.stream = None
         self.default_dtype = default_dtype
 
-    def empty(self, shape, dtype=self.default_dtype, name=None, allocator=drv.mem_alloc):
+    def empty(self, shape, dtype=None, name=None, allocator=drv.mem_alloc):
         """
         allocate the space for a GPUTensor
         """
+        dtype = self.default_dtype if dtype is None else dtype
         return GPUTensor(self, shape, dtype, allocator=allocator,
                           name=name, rounding=self.round_mode)
 
-    def array(self, ary, dtype=self.default_dtype, name=None, allocator=drv.mem_alloc):
+    def array(self, ary, dtype=None, name=None, allocator=drv.mem_alloc):
         """
         converts a numpy array to a GPUTensor
         """
+        dtype = self.default_dtype if dtype is None else dtype
         return GPUTensor(self, ary.shape, dtype, allocator=allocator,
                           name=name, rounding=self.round_mode).set(ary)
 
-    def zeros(self, shape, dtype=self.default_dtype, name=None, allocator=drv.mem_alloc):
+    def zeros(self, shape, dtype=None, name=None, allocator=drv.mem_alloc):
         """
         Returns an array of the given shape and dtype filled with 0's.
         """
+        dtype = self.default_dtype if dtype is None else dtype
         return GPUTensor(self, shape, dtype, allocator=allocator,
                           name=name, rounding=self.round_mode)._assign(0)
 
-    def ones(self, shape, dtype=self.default_dtype, name=None, allocator=drv.mem_alloc):
+    def ones(self, shape, dtype=None, name=None, allocator=drv.mem_alloc):
         """
         Returns an array of the given shape and dtype filled with 1's.
         """
+        dtype = self.default_dtype if dtype is None else dtype
         return GPUTensor(self, shape, dtype, allocator,
                           name=name, rounding=self.round_mode)._assign(1)
 
